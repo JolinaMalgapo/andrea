0fcceab43d374702e7c325309e83cadf783e9395
Malgapo <jolina.a.malgapo@accenture.com>
2018-04-13 17:43:04 +0800

    4th

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+$(document).ready(function() {
+ 
+	 $('.owl-carousel').owlCarousel({
+	    stagePadding: 50,
+	    loop:true,
+	    margin:20,
+    	responsiveClass:true,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:5
+	        }
+	    }
+	});
+ 
 });
